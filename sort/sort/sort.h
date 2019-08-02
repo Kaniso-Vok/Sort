3d9f090403893bc79ad7e264eb7d87fe4b048df2
sonissa <48381669+sonissa@users.noreply.github.com>
2019-08-02 15:31:07 +0800

    快速排序

---
+++ sort/sort/sort.h
@@ -0,0 +1,9 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+#include<stdio.h>
+#include<stdlib.h>
+void dealMergeSort(int *arr, int * tmp, int start, int end);
+void MergeSort(int * arr, int n);
+void QSort(int * arr, int n);
+
+#endif//_SORT_H
